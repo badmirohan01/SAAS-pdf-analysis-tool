5e45d32636469fc981456b7e5326d219ddabf703
Rohan Badmi <rohanbadmi@gmail.com>
2025-08-10 19:24:03 +0530

    Setup webhooks, Prisma and Supabase

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,35 @@
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const evt = await verifyWebhook(req);
+
+    // Do something with payload
+    // For this guide, log payload to console
+    const { id } = evt.data;
+    const eventType = evt.type;
+    console.log(
+      `Received webhook with ID ${id} and event type of ${eventType}`
+    );
+    console.log("Webhook payload:", evt.data);
+
+    if (evt.type === "user.created") {
+      const { id, email_addresses, first_name, last_name } = evt.data;
+      try {
+        const fullname = `${first_name || ""} ${last_name || ""}`.trim();
+
+        //todo : call prisma to create user
+        console.log(`User with ${id} was inserted into database`);
+      } catch (error) {
+        console.log("Error saving user to database", error);
+        return new Response("Error saving user", { status: 500 });
+      }
+    }
+
+    return new Response("Webhook received", { status: 200 });
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
+  }
+}
