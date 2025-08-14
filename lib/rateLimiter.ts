d5cad53880db32ce4cbaeb8e34111e1995fc99dd
Rohan Badmi <rohanbadmi@gmail.com>
2025-08-14 23:59:23 +0530

    Deployment Fix

--- lib/rateLimiter.ts
+++ lib/rateLimiter.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { LRUCache } from "lru-cache";
 import { RATE_LIMIT } from "./constants";
 import { ApiError } from "./errors";
